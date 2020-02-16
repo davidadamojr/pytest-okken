ed132d3a726912e00918fcad21b149c448b46439
David Adamo Jr <me@davidadamojr.com>
2020-02-16 13:08:21 -0500

    Initial commit

---
+++ tests/unit/test_task_fail.py
@@ -0,0 +1,16 @@
+"""Use the Task type to show test failures."""
+from tasks import Task
+
+
+def test_task_equality():
+    """Different tasks should not be equal."""
+    t1 = Task('sit there', 'brian')
+    t2 = Task('do something', 'okken')
+    assert t1 == t2
+
+
+def test_dict_equality():
+    """Different tasks compared as dicts should not be equal."""
+    t1 = Task('sit there', 'brian')
+    t2 = Task('do something', 'okken')
+    assert t1 == t2
