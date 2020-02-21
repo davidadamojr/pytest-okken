0292c7ef259c9b6ba38c5b5b63af4da5351043cc
David Adamo Jr <me@davidadamojr.com>
2020-02-21 03:21:46 -0500

    fixtures

--- tests/func/test_add.py
+++ tests/func/test_add.py
@@ -3,20 +3,8 @@ import tasks
 from tasks import Task
 
 
-@pytest.fixture(autouse=True)
-def initialized_tasks_db(tmpdir):
-    """Connect to db before testing, disconnect after."""
-    # Setup : start db
-    tasks.start_tasks_db(str(tmpdir), 'tiny')
-
-    yield # this is where the testing happens
-
-    # Teardown : stop db
-    tasks.stop_tasks_db()
-
-
-def test_add_returns_valid_id():
-    """tasks.add(<valid task> should return an integer."""
+def test_add_returns_valid_id(tasks_db):
+    """tasks.add(<valid task>) should return an integer."""
     # GIVEN an initialized tasks db
     # WHEN a new task is added
     # THEN returned task_id is of type int
